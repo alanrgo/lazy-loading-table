e6046417c862d948503cf838821403b0f3c4ec86
Alan Gomes <alan.gomes@thoughtworks.com>
2020-05-21 00:19:45 -0300

    add ascending sorting behavior into columns

--- src/components/InfiniteTable/PeopleTableList/PeopleTableList.js
+++ src/components/InfiniteTable/PeopleTableList/PeopleTableList.js
@@ -1,20 +1,60 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from 'prop-types';
 import './PeopleTableList.css'
+import _ from 'lodash'
 
 const PeopleTableList = ({people}) => {
+
+  const [ sortName, setSortName ] = useState(undefined)
+  const [ sortEmail, setSortEmail ] = useState(undefined)
+  const [ sortCity, setSortCity ] = useState(undefined)
   
+  const handleOnSortingClick = (column) => {
+    const columnOptions = [
+      {
+        name: 'name',
+        hook: setSortName
+      },
+      {
+        name: 'email',
+        hook: setSortEmail
+      },
+      {
+        name: 'city',
+        hook: setSortCity
+      },
+    ]
+    
+    columnOptions.forEach(option => {
+      if(option.name === column ){
+        option.hook('asc')
+      } else {
+        option.hook(undefined)
+      }
+    })
+  }
+  
+  let processedPeopleData = people
+  
+  if( sortName ) 
+    processedPeopleData = _.sortBy(processedPeopleData, 'name', [sortName])
+  if( sortEmail )
+    processedPeopleData = _.sortBy(processedPeopleData, 'email', [sortEmail])
+  if( sortCity )
+    processedPeopleData = _.sortBy(processedPeopleData, 'city', [sortCity])
+  
+
   return (
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>City</th>
+            <th onClick={() => handleOnSortingClick('name')}>Name</th>
+            <th onClick={() => handleOnSortingClick('email')}>Email</th>
+            <th onClick={() => handleOnSortingClick('city')}>City</th>
           </tr>
         </thead>
         <tbody>
-        {people.map(person => {
+        {processedPeopleData.map(person => {
           return (
               <tr key={`${person.name}-tr`}>
                 <td key={person.name}>{ person.name }</td>
